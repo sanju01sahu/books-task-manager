7bf33e66518953b57c275996582c524cae279233
Sanjay Sahu <77436131+sanju01sahu@users.noreply.github.com>
2024-04-23 13:43:23 +0530

    Insert: api route

--- frontend/src/api/index.jsx
+++ frontend/src/api/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://books-task-manager.onrender.com/api",
+  baseURL: "https://calm-rose-pangolin-tux.cyclic.app/api",
 });
 export default api;
