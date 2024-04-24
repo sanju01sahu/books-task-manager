96a07f35bbb79036f5529cc827b9f50831237535
Sanjay Sahu <77436131+sanju01sahu@users.noreply.github.com>
2024-04-25 00:20:19 +0530

    changes

--- backend/routes/task.route.js
+++ backend/routes/task.route.js
@@ -6,7 +6,7 @@ const TaskModel = require("../models/task.model");
 const errorHandler = require("../middlewares/error.middleware");
 const router = express.Router();
 
-// Routes beginning with /api/tasks
+// Routes beginning with /tasks/
 
 router.get("/", authenticateUser, async (req, res) => {
   try {
@@ -34,6 +34,37 @@ router.get("/:taskId", authenticateUser, async (req, res) => {
     res
       .status(200)
       .json({ task, status: true, msg: "Task found successfully.." });
+  } catch (err) {
+    console.error(err);
+    return res
+      .status(500)
+      .json({ status: false, msg: "Internal Server Error" });
+  }
+});
+
+router.get("/search", authenticateUser, async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1; // Default to page 1 if not specified
+    const limit = parseInt(req.query.limit) || 25; // Default limit to 4 tasks per page
+
+    const query = buildQuery(req.query, req.user.id);
+
+    const totalCount = await TaskModel.countDocuments(query); // Total count of tasks
+
+    const tasks = await TaskModel.find(query)
+      .skip((page - 1) * limit) // Skip items based on current page and limit
+      .limit(limit); // Limit the number of items returned per page
+
+    const totalPages = Math.ceil(totalCount / limit); // Calculate total pages
+
+    res.status(200).json({
+      tasks,
+      page,
+      totalPages,
+      totalCount,
+      status: true,
+      msg: "Tasks retrieved successfully..",
+    });
   } catch (err) {
     console.error(err);
     return res
@@ -98,5 +129,20 @@ router.delete("/:taskId", authenticateUser, async (req, res) => {
       .json({ status: false, msg: "Internal Server Error" });
   }
 });
+
+function buildQuery(queryParams, userId) {
+  const query = { user: userId };
+
+  if (queryParams.search) {
+    const searchRegex = new RegExp(queryParams.search, "i");
+    query.description = searchRegex;
+  }
+
+  if (queryParams.status) {
+    query.status = queryParams.status;
+  }
+
+  return query;
+}
 
 module.exports = router;
